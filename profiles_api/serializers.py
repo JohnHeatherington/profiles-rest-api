2e8ded59adb3915b2c8e772955a5a28d3410c5a1
John Heatherington <john.heatherington@corfinancialgroup.com>
2024-08-06 11:48:58 +0100

    Added Post, PUT, PATCH & DELETE methods to API view

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -3,5 +3,4 @@ from rest_framework import serializers
 
 class HelloSerializer(serializers.Serializer):
     """Serializes a name field for testing the APIView"""
-    name = serializers.Charfield(max_length=10)
-    
+    name = serializers.CharField(max_length=10)
